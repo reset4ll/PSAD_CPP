bb6418b0bc70e448ac51d02dab48b5611e72da5e
Antonio Esteban <reset4ll@yahoo.es>
2021-06-04 17:28:54 +0200

    subido ex47_2.cpp (sección 4.7)

---
+++ Chapter04/ex47_2.cpp
@@ -0,0 +1,35 @@
+// Programa para encontrar la última letra alfabéticamente
+
+#include <iostream>
+
+// Prototipo
+char get_last(char, char);
+
+int main()
+{
+    char char1, char2, char3; // Letras para ser procesadas
+    char is_last;  // Última letra ordenada alfabéticamente
+
+    // Lee las tres letras
+    std::cout << "Introduzca tres letras cualquiera: ";
+    std::cin >> char1 >> char2 >> char3;
+
+    // Guarda alfabéticamente la última de las dos en get_last()
+    is_last = get_last(char1, char2);
+
+    // Guarda alfabéticamente la última entre is_last y char3
+    is_last = get_last(char3, is_last);
+
+    // Muestra el resultado
+    std::cout << is_last << " es la última letra en orden alfabético." \
+              << std::endl;
+}
+
+// Definición de last_get() que encuentra la última letra de las dos
+char get_last(char letter1, char letter2)
+{
+    if (letter1 > letter2)
+        return letter1;
+    else
+        return letter2;
+}
