fd8dccfc944d9621bdbbb9531c90fef275017c64
Antonio Esteban <reset4ll@yahoo.es>
2021-06-25 14:03:18 +0200

    subidos ex61_1 (sección 6.1)

---
+++ Chapter06/ex61_1.cpp
@@ -0,0 +1,31 @@
+// Escribir un controlador para la función 'hypot()'
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "hipot.h"
+
+// Prototipo de la función
+double hipot(double, double);
+
+int main()
+{
+    double number1, number2, hip;
+
+    std::cout << "Introduzca los valores de los catetos: ";
+    std::cin >> number1 >> number2;
+
+    // Analiza si los números son positivos
+    if ((number1 < 0) || (number2 < 0)) {
+            std::cout << "Los números han de ser positivos!" << '\n';
+            std::cout << "Pruebe otra vez." << '\n';
+            return 0;
+            }
+
+    hip = hipot(number1, number2); // Calcula la hipotenusa
+
+    std::cout << "La hipotenusa vale: " << std::setprecision(3) 
+              << hip << '\n';
+
+    return 0;
+}
---
+++ Chapter06/hipot.h
@@ -0,0 +1,13 @@
+// Define la función hipotenusa
+
+#ifndef HIPOT_H
+#define HIPOT_H
+
+#include <cmath>
+
+double hipot(double x, double y)
+{
+    return sqrt(x * x + y * y);
+}
+
+#endif
