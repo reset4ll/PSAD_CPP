e4ec6f58403014d90d6674a41de03aee19cf52fd
Antonio Esteban <reset4ll@yahoo.es>
2021-06-26 20:29:56 +0200

    subido ex62_3.cpp (sección 6.2)

---
+++ Chapter06/ex62_3.cpp
@@ -0,0 +1,49 @@
+// Escribir una función que eleve un número 'double'(X) a una potencia 'int'(N)
+//  al multiplicar X por si mismo N veces usando un 'for' loop.
+//  Para hacer que la función funcione para valores negativos de N usar la 
+//  función 'abs(N)' como la expresión final del bucle 'for'
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+// Prototipo de la función
+double my_pow(double, int);
+
+
+int main()
+{
+    int index;  // Itereador
+
+    for (index = 0; index <= 5; ++index)  // Cinco números
+
+    // Función my_pow() :: base (+,-) y exp (+)
+    std::cout << "Index: " << index << " -> " << "my_pow(2,exp): " \
+              << my_pow(2,index) << std::setw(5)  << " -> " << "my_pow(-2,-exp): "  \
+              << my_pow(-2,-index) << std::setw(6) << '\n';
+
+    std::cout << "----------" << '\n';
+
+    // Función pow() :: test my_pow() 
+    for (index = 0; index <= 5; ++index)  // Cinco números
+      
+    std::cout << "Index: " << index << " -> " << "pow(2,exp): " \
+              << pow(2,index) << std::setw(5)  << " -> " << "pow(-2,-exp): "  \
+              << pow(-2,-index) << std::setw(6) << '\n';
+    
+    return 0;
+}
+
+// Definición de my_pow()
+double my_pow(double base, int exp)
+{
+    int index;
+    double potencia = 1;
+
+    for (index = 1; index <= exp; ++index)
+        potencia *= base;
+    return potencia;
+
+
+
+}
